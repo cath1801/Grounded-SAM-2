cb840bdc81bde50babc778c09ad290a07ab502cd
cath1801 <78136789+cath1801@users.noreply.github.com>
2025-07-08 14:20:33 -0500

    changed installation checkpoint files

--- gdino_checkpoints/download_ckpts.sh
+++ gdino_checkpoints/download_ckpts.sh
@@ -7,18 +7,29 @@
 # LICENSE file in the root directory of this source tree.
 
 
+# Use either wget or curl to download the checkpoints
+if command -v wget &> /dev/null; then
+    CMD="wget"
+elif command -v curl &> /dev/null; then
+    CMD="curl -L -O"
+else
+    echo "Please install wget or curl to download the checkpoints."
+    exit 1
+fi
+
 # Define the URLs for the checkpoints
 BASE_URL="https://github.com/IDEA-Research/GroundingDINO/releases/download/"
 swint_ogc_url="${BASE_URL}v0.1.0-alpha/groundingdino_swint_ogc.pth"
-swinb_cogcoor_url="${BASE_URL}v0.1.0-alpha2/groundingdino_swinb_cogcoor.pth"
+# swinb_cogcoor_url="${BASE_URL}v0.1.0-alpha2/groundingdino_swinb_cogcoor.pth"
 
 
 
 # Download each of the four checkpoints using wget
 echo "Downloading groundingdino_swint_ogc.pth checkpoint..."
-wget $swint_ogc_url || { echo "Failed to download checkpoint from $swint_ogc_url"; exit 1; }
+$CMD $swint_ogc_url || { echo "Failed to download checkpoint from $swint_ogc_url"; exit 1; }
 
-echo "Downloading groundingdino_swinb_cogcoor.pth checkpoint..."
-wget $swinb_cogcoor_url || { echo "Failed to download checkpoint from $swinb_cogcoor_url"; exit 1; }
+
+# echo "Downloading groundingdino_swinb_cogcoor.pth checkpoint..."
+# wget $swinb_cogcoor_url || { echo "Failed to download checkpoint from $swinb_cogcoor_url"; exit 1; }
 
 echo "All checkpoints are downloaded successfully."
